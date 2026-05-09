1cce6ffdfce157112ab3dd0f0b61537b07fe3f66
Paul Jomy <49817584+pauljomy@users.noreply.github.com>
2026-05-09 02:34:49 +0000

    working through db env files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,11 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   dbconfig
+}
+
+type dbconfig struct {
+	dsn string
 }
 
 type application struct {
@@ -22,11 +32,13 @@ type application struct {
 }
 
 func main() {
+
+	godotenv.Load()
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
-
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost:5432/greenlight", "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
@@ -36,6 +48,14 @@ func main() {
 		logger: logger,
 	}
 
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error("Failed to open database", "error", err)
+		os.Exit(1)
+	}
+
+	defer db.Close()
+	logger.Info("Database connection established")
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
@@ -46,10 +66,28 @@ func main() {
 
 	logger.Info("Starting server", "port", cfg.port, "env", cfg.env, "version", version)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		logger.Error("Failed to start server", "error", err)
 		os.Exit(1)
 	}
+
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 
 }
